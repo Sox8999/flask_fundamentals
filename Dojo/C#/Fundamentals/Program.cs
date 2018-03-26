03ce52d83715dbe50506723f0b64ea6c24c6b535
Sox8999 <bwroeben@gmail.com>
2018-03-25 19:23:09 -0700

    C#

---
+++ Dojo/C#/Fundamentals/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Fundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i=1; i<256; i++)
+            {
+            Console.WriteLine(i);
+            }
+
+            for (int j = 1; j < 101 ; j++)
+
+                if (j % 3 == 0 && j % 5 == 0)
+                {
+                    Console.WriteLine(j + " " +  "FizzBuzz");
+                }
+            
+                else if (j % 3 == 0)
+                {
+                    Console.WriteLine(j + " " + "Fizz");
+                }
+            
+                else if (j % 5 == 0)
+                { 
+                    Console.WriteLine(j + " " + "Buzz");
+                }
+               
+        }
+    }
+}
